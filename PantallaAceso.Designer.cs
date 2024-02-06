542056e516dfebe940df52bc100212b6653b6e9d
adalberto fm <adalbertofm1995@gmail.com>
2024-02-06 07:58:48 -0600

    init

---
+++ PantallaAceso.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace KioscoConsulta
+{
+    partial class PantallaAcceso
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PantallaAcceso));
+            this.BtIngreso = new System.Windows.Forms.Button();
+            this.TbUsuario = new System.Windows.Forms.TextBox();
+            this.lblNombre_FA = new System.Windows.Forms.Label();
+            this.lblNumEmp_FA = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtIngreso
+            // 
+            this.BtIngreso.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtIngreso.Location = new System.Drawing.Point(246, 521);
+            this.BtIngreso.Margin = new System.Windows.Forms.Padding(6);
+            this.BtIngreso.Name = "BtIngreso";
+            this.BtIngreso.Size = new System.Drawing.Size(286, 100);
+            this.BtIngreso.TabIndex = 0;
+            this.BtIngreso.Text = "INGRESAR";
+            this.BtIngreso.UseVisualStyleBackColor = true;
+            this.BtIngreso.Click += new System.EventHandler(this.BtIngreso_Click);
+            // 
+            // TbUsuario
+            // 
+            this.TbUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbUsuario.Location = new System.Drawing.Point(39, 541);
+            this.TbUsuario.Margin = new System.Windows.Forms.Padding(6);
+            this.TbUsuario.Name = "TbUsuario";
+            this.TbUsuario.Size = new System.Drawing.Size(178, 75);
+            this.TbUsuario.TabIndex = 1;
+            this.TbUsuario.UseSystemPasswordChar = true;
+            // 
+            // lblNombre_FA
+            // 
+            this.lblNombre_FA.AutoSize = true;
+            this.lblNombre_FA.Location = new System.Drawing.Point(286, 52);
+            this.lblNombre_FA.Name = "lblNombre_FA";
+            this.lblNombre_FA.Size = new System.Drawing.Size(133, 31);
+            this.lblNombre_FA.TabIndex = 2;
+            this.lblNombre_FA.Text = "NOMBRE";
+            // 
+            // lblNumEmp_FA
+            // 
+            this.lblNumEmp_FA.AutoSize = true;
+            this.lblNumEmp_FA.Location = new System.Drawing.Point(80, 52);
+            this.lblNumEmp_FA.Name = "lblNumEmp_FA";
+            this.lblNumEmp_FA.Size = new System.Drawing.Size(149, 31);
+            this.lblNumEmp_FA.TabIndex = 3;
+            this.lblNumEmp_FA.Text = "NUM_EMP";
+            // 
+            // PantallaAcceso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.lblNumEmp_FA);
+            this.Controls.Add(this.lblNombre_FA);
+            this.Controls.Add(this.TbUsuario);
+            this.Controls.Add(this.BtIngreso);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "PantallaAcceso";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kiosco_información";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtIngreso;
+        private System.Windows.Forms.TextBox TbUsuario;
+        public System.Windows.Forms.Label lblNombre_FA;
+        public System.Windows.Forms.Label lblNumEmp_FA;
+    }
+}
+
---
+++ PantallaAceso.cs
@@ -0,0 +1,83 @@
+﻿using KioscoConsulta.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KioscoConsulta
+{
+    public partial class PantallaAcceso : Form
+    {
+        AN_COMEDOR_USUARIO oUsuario = null; 
+
+        public PantallaAcceso()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtIngreso_Click(object sender, EventArgs e)
+        {
+            string usuarioComedor = TbUsuario.Text;
+
+            if (CheckUser(usuarioComedor) != true)
+            {
+                //abrir modal usuario no valido.
+                TbUsuario.Text = "";
+                TbUsuario.Focus();
+            }
+            else
+            {//abrir la otra pagina//
+             //enviar código y nombre.
+
+                PasarUsuarioForms();
+                TbUsuario.Text = "";
+                TbUsuario.Select();
+            }
+
+        }
+
+        private bool CheckUser(string usuarioComedor)
+        {
+            bool encontrado = false;
+
+            using (Models.DBEntities db = new Models.DBEntities())
+            {
+                var userExist = from d in db.AN_COMEDOR_USUARIO
+                                where d.USUARIO_COMEDOR == usuarioComedor
+                                && d.F_BAJA == "" && d.F_SUSPENCION == ""
+                                select new {d.NUM_EMP,d.NOMBRE,d.AP_PATER,d.AP_MATER };
+                if (userExist.Count() ==1)
+                {
+                    encontrado = true;
+
+                    foreach (var user in userExist)
+                    {
+                        lblNumEmp_FA.Text = user.NUM_EMP; lblNumEmp_FA.Visible = false;
+                        lblNombre_FA.Text = user.NOMBRE + " " + user.AP_PATER + " " + user.AP_MATER; lblNombre_FA.Visible = false;
+                    }
+                }   
+            }
+            return encontrado;
+        }
+
+
+        private void PasarUsuarioForms()
+        {
+            PantallaMain FrmPantallaMain = new PantallaMain();
+            FrmPantallaMain.lblNombre_FM.Text = lblNombre_FA.Text;
+            FrmPantallaMain.lblNumEmp_FM.Text = lblNumEmp_FA.Text;
+            FrmPantallaMain.Show();
+        }
+
+    }
+}
